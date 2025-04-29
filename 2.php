3cb0789360ecb2acfa09180a996efa36b81ad1ce
M260 <41881281+M2600@users.noreply.github.com>
2025-04-29 21:28:48 +0900

    version2へのリンク

---
+++ 2.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="css/index.css">
+
+<?php
+if(!isset($_SESSION)){
+    session_start();
+}
+if(!isset($_SESSION["id"])){
+    header("Location: /login.php");
+}
+
+require("templates/editor.html");
+
+
+?>

d62ffb90162fe604f9641a6dd5e7029d84f8dade
m260 <shota.M.020626.S.K.F@gmail.com>
2025-07-31 23:19:13 +0900

    log user id to chat log

--- api/ai.php
+++ api/ai.php
@@ -1,6 +1,7 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
 
+session_start();
 // 設定
 $LMSTUDIO_API_URL = 'https://kanemune_ai.dolittle.cc/lmstudio/v1/chat/completions'; // lmstudioのAPIエンドポイント
 $API_KEY = '***REMOVED***'; // 必要に応じて環境変数や設定ファイルから取得
@@ -33,7 +34,7 @@ $logFile = $LOG_DIR . "chat.log";
 function log_chat_request($logFile, $input, $messages) {
     $ts = date('Y-m-d H:i:s');
     $ip = $_SERVER['REMOTE_ADDR'] ?? '-';
-    $user = isset($_SERVER['PHP_AUTH_USER']) ? $_SERVER['PHP_AUTH_USER'] : (isset($_SERVER['REMOTE_USER']) ? $_SERVER['REMOTE_USER'] : '-');
+    $user = isset($_SESSION['id']) ? $_SESSION['id'] : '-';
     $model = $input['model'] ?? 'default';
     $fileContext = isset($input['fileContext']) && !empty($input['fileContext']['content']) ? 'yes' : 'no';
     $log = [

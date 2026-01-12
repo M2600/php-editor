98a99fef5e114960a2d7df3aa7e7ece6827df8fe
m260 <shota.M.020626.S.K.F@gmail.com>
2026-01-13 00:33:08 +0900

    ba_authに教員ログイン検出の実装

--- test/ba_auth.php
+++ test/ba_auth.php
@@ -3,37 +3,46 @@ session_start();
 
 
 $code = isset($_GET['code']) ? $_GET['code'] : null;
+$originalState = isset($_SESSION['oauth_state']) ? $_SESSION['oauth_state'] : "";
+unset($_SESSION['oauth_state']);
 $returnedState = isset($_GET['state']) ? $_GET['state'] : null;
 if ($code !== null) {
 	// コールバック処理
-	if (!hash_equals($_SESSION['oauth_state'], $returnedState)) {
-		unset($_SESSION['oauth_state']);
-		echo "Invalid state parameter.";
+	if (!hash_equals($originalState, $returnedState)) {
+		echo "stateが一致しません。やり直してください。";
+		echo "<br><a href='ba_auth.php'>再試行</a>";
 		exit();
 	}
 
 	$verifyUrl = 'https://bitarrow3.eplang.jp/bitarrow/?Login/bauth&status=' . urlencode($code);
 	$response = file_get_contents($verifyUrl);
 	if ($response === "NG") {
-		echo "Authentication failed: " . htmlspecialchars($response);
-		header('Location: ba_auth.php');
+		echo "認証に失敗しました: " . htmlspecialchars($response);
+		echo "<br><a href='ba_auth.php'>再試行</a>";
 		exit();
 	}
 
 	// verification成功の場合
+	// 学生ログインの場合、codeに'user','class'が含まれる
+	// 教員ログインの場合、codeに'teacher'が含まれる
+	// 教員で代理ログインしていると'teacher','user','class'が全て含まれる
 	$data = json_decode($code, true);
-	if (!$data['user'] || !$data['class']) {
-		echo "BitArrowのアカウント情報が取得できません。";
-
-		// BAでログインさせる処理k
-		$baLoginUrl = 'https://bitarrow3.eplang.jp/bitarrow/?Login/form';
-		echo '<br><a href="' . htmlspecialchars($baLoginUrl) . '">BitArrowでログイン</a>後に再度アクセスしてください。';
+	if ($data['teacher']) {
+		// 教員としてログイン
+		echo "教員として認証成功!<br>";
+	}
+	else if ($data['user'] && $data['class']) {
+		// 学生としてログイン
+		echo "学生として認証成功!<br>";
+	}
+	else {
+		echo "認証データが無効です。<br>";
 		exit();
 	}
-	
+
+	// 認証成功
 	session_regenerate_id(true);
-	echo "Authentication successful!<br>";
-	echo 'Your identifier: ' . $code;
+	echo 'ログイン情報: ' . $code . "<br>";
 
 	exit();
 }
@@ -42,7 +51,7 @@ if ($code !== null) {
 $state = bin2hex(random_bytes(16));
 $callbackUrl = (empty($_SERVER['HTTPS']) ? 'http://' : 'https://') . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . '?' . 'state=' . $state;
 $_SESSION['oauth_state'] = $state;
-$authUrl = 'https://bitarrow3.eplang.jp/bitarrow/?Login/curStatus=&otp=1&callback=' . urlencode($callbackUrl);
+$authUrl = 'https://bitarrow3.eplang.jp/bitarrow/?Login/curStatus&otp=1&callback=' . urlencode($callbackUrl);
 header('Location: ' . $authUrl);
 exit();
 ?>
